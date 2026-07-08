fa51d5c0792c93df6cb4df47783b9d0ff15fb2b6
korelina_oa <korelina_oa@astramed-ms.ru>
2026-07-08 18:47:39 +0500

    0.1.0: добавлен шаблон класса кастомного указателя для работы с gstreamer

---
+++ gst_ptr.h
@@ -0,0 +1,19 @@
+#ifndef GST_PTR_H
+#define GST_PTR_H
+#include <utility>
+
+template <typename T, void (*Deleter)(T*)>
+class GstPtr {
+public:
+    constexpr GstPtr() noexcept : m_ptr(nullptr) {}
+    explicit GstPtr(T* ptr) noexcept : m_ptr(ptr) {}
+
+    ~GstPtr() noexcept {
+        reset();
+    }
+
+private:
+    T* m_ptr = nullptr;
+};
+
+#endif // GST_PTR_H
