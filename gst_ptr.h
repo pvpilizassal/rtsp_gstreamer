63673b39c638e0574e51af2b84069de9a6c4099f
korelina_oa <korelina_oa@astramed-ms.ru>
2026-07-09 18:03:00 +0500

    0.2.0: добавлен файл gst_types.h для объявления структуры деструкторов элементов gstreamer и объявления псевдонимов элементов через обертку GstPtr

--- gst_ptr.h
+++ gst_ptr.h
@@ -12,11 +12,43 @@ public:
         reset();
     }
 
+    GstPtr(const GstPtr&) = delete; //запрет копирования (двойное освобождение памяти)
+    GstPtr& operator=(const GstPtr&) = delete; //запрет присваивания
+
+    // разрешение перемещения (разрешенное копирование)
+    GstPtr(GstPtr&& other) noexcept : m_ptr(std::exchange(other.m_ptr, nullptr)) {}
+
+    // разрешение перемещения через =
+    GstPtr& operator=(GstPtr&& other) noexcept {
+        if (this != &other) {
+            reset();
+            m_ptr = std::exchange(other.m_ptr, nullptr);
+        }
+        return *this;
+    }
+
+    T* operator->() const noexcept { return m_ptr; }
+    explicit operator bool() const noexcept { return m_ptr != nullptr; }
+
     void reset(T* ptr = nullptr) noexcept {
         if (m_ptr) {
             Deleter(m_ptr);
         }
         m_ptr = ptr;
+    }
+
+    // возврат сырого указателя на объект в m_ptr и зануление оного, чтоб не освободить
+    [[nodiscard]] T* release() noexcept {
+        return std::exchange(m_ptr, nullptr);
+    }
+
+    // возврат указателя на объект в куче
+    [[nodiscard]] T* get() const noexcept { return m_ptr; }
+
+    // возврат указателя на пустой и зануленный указатель
+    T** put() noexcept {
+        reset();
+        return &m_ptr;
     }
 
 private:
---
+++ gst_types.h
@@ -0,0 +1,23 @@
+#ifndef GST_TYPES_H
+#define GST_TYPES_H
+
+#include <gst/gst.h>
+#include "gst_ptr.h"
+
+// void* = gpointer
+namespace GstDetails {
+inline void element_unref(GstElement* p) { if (p) gst_object_unref(p); }
+inline void bus_unref(GstBus* p)         { if (p) gst_object_unref(p); }
+inline void caps_unref(GstCaps* p)       { if (p) gst_caps_unref(p); }
+inline void message_unref(GstMessage* p) { if (p) gst_message_unref(p); }
+inline void g_free_char(gchar* p)        { if (p) g_free(p); }
+}
+
+// псевдонимы элементов и их деструкторов с использованием обертки
+using GstElementPtr = GstPtr<GstElement, GstDetails::element_unref>;
+using GstBusPtr     = GstPtr<GstBus,     GstDetails::bus_unref>;
+using GstCapsPtr    = GstPtr<GstCaps,    GstDetails::caps_unref>;
+using GstMessagePtr = GstPtr<GstMessage, GstDetails::message_unref>;
+using GcharPtr      = GstPtr<gchar,      GstDetails::g_free_char>;
+
+#endif // GST_TYPES_H
