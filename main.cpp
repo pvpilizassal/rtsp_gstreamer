ad602cae28b0524dde6515b1b4535597bd7eb2f7
korelina_oa <korelina_oa@astramed-ms.ru>
2026-07-09 18:15:12 +0500

    test: тест создания элемента GstElementPtr и проверка на утечки пройден

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include "mainwindow.h"
+#include "gst_types.h"
 #include <QApplication>
+#include <QDebug>
+#include <iostream>
+
+void runTest() {
+    for (int i = 0; i < 100000; ++i) {
+        GstElementPtr element(gst_element_factory_make("fakesink", nullptr));
+        if (!element)
+            return;
+    }
+
+    qDebug() << "Test complete";
+}
 
 int main(int argc, char *argv[])
 {
+    gst_init(&argc, &argv);
+    runTest();
+
     QApplication app(argc, argv);
     MainWindow window;
     window.show();
